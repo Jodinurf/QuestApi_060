90bc9f8d5667b0f1f0460a4ef8f72a20c5a56fdd
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-12-30 07:03:30 +0700

    Finished

---
+++ app/src/main/java/com/jodifrkh/serverdatabase/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.jodifrkh.serverdatabase
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.jodifrkh.serverdatabase.ui.theme.ServerDatabaseTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ServerDatabaseTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    MahasiswaApp()
+                }
+            }
+        }
+    }
+}
